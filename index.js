a7f57ddbbeb29eb6c23eb33e7783a99808710fe5
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-10-15 17:05:48 +0000

    up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const btnEl = document.getElementById("calculate");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
@@ -9,5 +10,6 @@ function calculateTotal() {
   const totalValue = billValue * (1 + tipValue / 100);
   totalSpan.innerText = totalValue.toFixed(2);
 }
+
 
 btnEl.addEventListener("click", calculateTotal);
